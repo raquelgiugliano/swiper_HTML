b89802dfd51db086813b18f7e2ef5d541cea5383
raquelgiugliano <raquel.giugliano@gmail.com>
2023-06-05 13:07:34 +0200

    Swiper

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div>
+      <h1>Swiper</h1>
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
